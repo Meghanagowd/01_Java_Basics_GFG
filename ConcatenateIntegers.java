a02459fdc67c5dab8dee0dfdbaf5b5b0ae7bf245
Meghanagowd <gowdameghana256@gmail.com>
2026-02-17 21:57:27 +0530

    Add Java Concatenate Integers program

---
+++ ConcatenateIntegers.java
@@ -0,0 +1,13 @@
+class ConcatenateIntegers {
+
+    public static void main(String[] args) {
+
+        // Declare integers
+        int num1 = 56;
+        int num2 = 5;
+ 
+
+        // Print result
+        System.out.println("Concatenated Result: "+num1+num2);
+    }
+}
