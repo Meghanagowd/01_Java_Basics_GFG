a9075c139023afa71309267311faa8f94bf0a08a
Meghanagowd <gowdameghana256@gmail.com>
2026-02-17 21:55:21 +0530

    Add Java Last Digit program

---
+++ LastDigit.java
@@ -0,0 +1,14 @@
+class LastDigit {
+
+    public static void main(String[] args) {
+
+        // Declare number
+        int number = 123;
+
+        
+         int lastDigit = number % 10;
+
+        // Print result
+        System.out.println("Last Digit: "+lastDigit );
+    }
+}
